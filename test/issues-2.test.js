df25025945889dbeda11e561aac8fdbc8d69e86e
stuneak <stuneak@gmail.com>
2019-12-29 22:21:24 +0300

    refactoring

--- test/issues-2.test.js
+++ test/issues-2.test.js
@@ -2,16 +2,16 @@ const createColor = require("../lib");
 const getColor = require("color");
 
 const str = [
-  '32ed',
-  '30z9-0-f9f0s',
-  '3999',
-  '490f990-fdz',
-  '9eff5f79-13ce-4555-bd86-1737a24ebe33',
-  '9fc91124-0c99-48f8-863e-49f173ead3f7'
+  "32ed",
+  "30z9-0-f9f0s",
+  "3999",
+  "490f990-fdz",
+  "9eff5f79-13ce-4555-bd86-1737a24ebe33",
+  "9fc91124-0c99-48f8-863e-49f173ead3f7"
 ];
 
-str.forEach((item) => {
-  test("Some strings return invalid hex values [https://github.com/stuneak/create-color/issues/2]", () => {
+str.forEach(item => {
+  test(`Some strings return invalid hex values [https://github.com/stuneak/create-color/issues/2] - ${item}`, () => {
     try {
       const color = createColor(item);
       getColor(color);
