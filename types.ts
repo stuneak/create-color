3617eac10135c8e1fe3322e880f21b50e30f3a73
Abu Shamsutdinov <stuneak@gmail.com>
2021-10-12 07:33:52 +0000

    add types

---
+++ types.ts
@@ -0,0 +1,7 @@
+declare module 'create-color' {
+  export interface CreateColorOptions {
+    format: string
+  }
+
+  export default function createColor(data: any, options?: CreateColorOptions): string
+}
